9057258ee4bbfdb2930aadc2fd4fdda093e3e335
Milena <milenatyagra.sc@gmail.com>
2023-05-21 23:01:44 -0300

    Criação de telas de login e home

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react"
+import Titulo from "../../components/common/titulo"
+import Carregamento from "../../components/layout/carregamento"
+
+import Informativo from "../../components/common/informativo"
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js"
+import { Pie } from "react-chartjs-2"
+
+import {
+  Projeto,
+  listarProjetos,
+} from "../../services/projetosService"
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+const Home: React.FC = () => {
+  const [carregando, setCarregando] = useState(false)
+  const [grafico, setGrafico] = useState({})
+  useEffect(() => {
+    buscaProjetos()
+  }, [])
+
+
+  const buscaProjetos = async () => {
+    setCarregando(true)
+    try {
+      const data = await listarProjetos()
+      formataGraficos(data)
+      console.log(data)
+    } catch (err) {
+      console.log(err)
+      alert("Ocorreu um erro, tente novamente")
+    } finally {
+      setCarregando(false)
+    }
+  }
+
+  const formataGraficos = (data: Projeto[]) => {
+    const serie: { [tec: string]: number } = {}
+
+    data.forEach((item: Projeto) => {
+      item.tecnologias.forEach((tec) => {
+        if (!serie[tec]) {
+          serie[tec] = 1
+        } else {
+          serie[tec] += 1
+        }
+      })
+    })
+    setGrafico(serie)
+  }
+
+  const data = {
+    labels: Object.keys(grafico),
+    datasets: [
+      {
+        label: "Projetos com a tecnologia",
+        data: Object.values(grafico),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  }
+  return (
+    <main>
+      <Titulo>Bem vindo ao nosso site</Titulo>
+      <Informativo >
+        <p>Tecnologias</p>
+        <Pie data={data} />
+      </Informativo>
+    </main>
+  )
+}
+
+export default Home
---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,76 @@
+import React from "react"
+import { LoginData, login as loginService } from "../../services/autenticacaoService"
+import { useAuth } from "../../context/AuthContext"
+
+import { useNavigate } from "react-router-dom"
+import * as Yup from "yup"
+
+import styles from "./Login.module.css"
+
+import Input from "../../components/common/forms/input"
+import Form from "../../components/common/forms/form"
+import Botao from "../../components/common/botao"
+import Titulo from "../../components/common/titulo"
+
+const initialValues: LoginData = {
+  email: "",
+  senha: "",
+}
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().email("E-mail inválido").required("E-mail é obrigatório"),
+  senha: Yup.string()
+    .min(6, "A senha deve ter pelo menos 6 caracteres")
+    .required("Senha é obrigatória"),
+})
+const Login = () => {
+  const navigate = useNavigate()
+  const { login } = useAuth()
+
+  const onSubmit = async (values: LoginData) => {
+    try {
+      const user = await loginService(values)
+      login(user)
+      navigate("/")
+    } catch (error) {
+      alert("erro")
+    }
+  }
+  return (
+    <div className={styles.loginWrapper}>
+      <Form
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        {({ errors, touched }) => (
+          <div className={styles.form}>
+            <Titulo>MEU SITE PESSOAL</Titulo>
+
+            <Input
+              label="Email"
+              name="email"
+              type="email"
+              errors={errors.email}
+              touched={touched.email}
+            />
+            <Input
+              label="Senha"
+              name="senha"
+              type="password"
+              errors={errors.senha}
+              touched={touched.senha}
+            />
+            <Botao
+              tipo="submit"
+            >
+              Entrar
+            </Botao>
+          </div>
+        )}
+      </Form>
+    </div>
+  )
+}
+
+export default Login
